61a65732df26156fae42a19e3593072d50af5b72
Duy <hi.am.not.me@gmail.com>
2019-03-16 09:10:59 +0700

    Add Open file function with line editor and notifications

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import os
 import struct
 from Crypto.Cipher import AES
 from Crypto import Random
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QMainWindow, QFileDialog
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QLabel, QMainWindow, QFileDialog, QLineEdit
 
 def Encrypt():
     print('Encrypt')
@@ -15,8 +16,11 @@ class Window(QMainWindow):
     def __init__(self):
         super().__init__()
         self.title = 'EndeCryptor'
-        self.width = 400
-        self.height = 250
+        self.width = 600
+        self.height = 375
+        self.file = None
+        self.lbl = None
+        self.txtEdit = None
         self.initGUI()
     
     def initGUI(self):
@@ -26,28 +30,55 @@ class Window(QMainWindow):
         self.resize(self.width, self.height)
         self.move(300, 300)
 
+        # textEdit
+        txtEdit = QLineEdit('', self)
+        txtEdit.move(20, 20)
+        txtEdit.setFixedWidth(450)
+        self.txtEdit = txtEdit
+
         # btnEncrypt
         btnEncrypt = QPushButton('Encrypt', self)
-        btnEncrypt.move(80, 100)
+        btnEncrypt.move(365, 325)
         btnEncrypt.setToolTip('Encryt the selected file')
         btnEncrypt.clicked.connect(self.openEncryptFile)
 
         # btnDecrypt
         btnDecrypt = QPushButton('Decrypt', self)
-        btnDecrypt.move(220, 100)
+        btnDecrypt.move(475, 325)
         btnDecrypt.setToolTip('Decryt the selected file')
         btnDecrypt.clicked.connect(self.openDecryptFile)
 
-        #lbl
-        lbl = QLabel('Choose your file to Encrypt or Decrypt', self)
+        # lbl
+        lbl = QLabel('No file seleted!', self)
+        lbl.move(20, 70)
         lbl.resize(400, 20)
+        self.lbl = lbl
+        
+
+        # open file button
+        btnOpenFile = QPushButton('Open', self)
+        btnOpenFile.move(475, 20)
+        btnOpenFile.setToolTip('Click to open a file')
+        btnOpenFile.clicked.connect(self.openFile)
+        
         self.show()
 
-    def openEncryptFile(self):
+    def openFile(self):
         options = QFileDialog.Options()
         options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Encrypt", "","All Files (*);;Python Files(*.py)", options=options)
-        if fileName:
+        fileName, _ = QFileDialog.getOpenFileName(self,"Choose a file", "","All Files (*);;Python Files(*.py)", options=options)
+        self.file = fileName
+        self.txtEdit.setText(self.file)
+        if self.lbl.text() == 'Please choos a file first!':
+            self.lbl.setText('Proceed')
+            self.lbl.setStyleSheet('color: black')
+        else:
+            self.lbl.setText('File chosen!')
+            self.lbl.setStyleSheet('color: black')
+
+    def openEncryptFile(self):
+        if self.file:
+            fileName = self.file
             print("Encrypt " + fileName)
             # Create encryptor
             iv = Random.new().read(16)
@@ -73,12 +104,16 @@ class Window(QMainWindow):
                             data += b' ' * (16 - n % 16) # fill in last block with spaces
                         encryptedData = encryptor.encrypt(data)
                         fout.write(encryptedData)
+            self.lbl.setText('File has been encrypted successfully!')
+            self.lbl.setStyleSheet('color: green')
+
+        else:
+            self.lbl.setText('Please choose a file first!')
+            self.lbl.setStyleSheet('color: red')
  
     def openDecryptFile(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getOpenFileName(self,"Decrypt", "","All Files (*);;Python Files(*.py)", options=options)
-        if fileName:
+        if self.file:
+            fileName = self.file
             print("Decrypt " + fileName)
             with open(fileName, 'rb') as fin:
                 # Read meta data
@@ -101,6 +136,12 @@ class Window(QMainWindow):
                         else:
                             fout.write(decd[:fsz]) # remove last spaces
                         fsz -= n
+            self.lbl.setText('File has been decrypted successfully!')
+            self.lbl.setStyleSheet('color: green')
+
+        else:
+            self.lbl.setText('Please choose a file first!')
+            self.lbl.setStyleSheet('color: red')
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
